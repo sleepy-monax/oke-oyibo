12fdab842e8c67f42c67727db14c0801f2dae3e4
VAN BOSSUYT Nicolas <nicolas.van.bossuyt@gmail.com>
2020-10-06 11:42:21 +0200

    glue: Disabled keyboard navigation.

--- source/core/glue/Glue.cpp
+++ source/core/glue/Glue.cpp
@@ -85,8 +85,7 @@ namespace core::glue
         ImGui::CreateContext();
 
         ImGuiIO &io = ImGui::GetIO();
-        io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard; // Enable Keyboard Controls
-        io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;     // Enable Docking
+        io.ConfigFlags |= ImGuiConfigFlags_DockingEnable; // Enable Docking
 
         ImGui::StyleColorsDark();
 
