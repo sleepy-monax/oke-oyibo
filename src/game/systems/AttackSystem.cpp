627cc31819f7271edf47202eabdeebd36b21374f
AiIurus <mathieu.trempont@hotmail.com>
2020-12-06 10:04:33 +0100

    Game: Fix with sword

--- src/game/systems/AttackSystem.cpp
+++ src/game/systems/AttackSystem.cpp
@@ -18,31 +18,30 @@ namespace game
             auto enemy = world.entities().view<game::Enemy, base::Position, game::Health, game::Attack>();
             auto player = world.entities().view<base::Player, base::Position, game::Health, game::Inventory, game::HoldItem, game::Attack>();
 
-            enemy.each([&](auto const &entity, auto &, auto &positionEnemy, auto &health_enemy, auto &attackEnemy) {
+            enemy.each([&](auto const &entity, auto &, auto &positionEnemy, auto &healthEnemy, auto &attackEnemy) {
                 auto enemy_pos = positionEnemy.pos2d();
-
-                player.each([&](auto &, auto &positionPlayer, auto &health_player, auto &inv, auto &hold, auto &attackPlayer) {
+                player.each([&](auto &, auto &positionPlayer, auto &healthPlayer, auto &inv, auto &hold, auto &attackPlayer)
+                {
                     auto player_pos = positionPlayer.pos2d();
 
                     if (enemy_pos.distance_to(player_pos) <= 5)
                     {
-                        health_player.current -= attackEnemy.attack;
+                        healthPlayer.current -= attackEnemy.attack;
 
                         if (IsKeyPressed(KEY_F))
                         {
-                            string holdItem = inv.inventory[hold.index].getItem().getName();
-
-                            if (holdItem == "sword")
-                            {
-                                attackPlayer.attack = 5;
+                            if (inv.inventory.size() != 0) {
+                                string holdItem = inv.inventory[hold.index].getItem().getName();
+                                if (holdItem.compare("sword") == 0) {
+                                    attackPlayer.attack = 5;
+                                }
                             }
-                            if (health_enemy.current - attackPlayer.attack < 0)
-                            {
+                            if (healthEnemy.current - attackPlayer.attack < 0){
                                 world.remove_entity(entity);
                             }
                             else
                             {
-                                health_enemy.current -= attackPlayer.attack;
+                                healthEnemy.current -= attackPlayer.attack;
                             }
                         }
                     }
